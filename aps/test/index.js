c3cd86d77201f0a4843699406efd539d858659c2
fabioriva <f.riva@sotefin.ch>
2025-02-10 08:33:02 +0000

    export default main

--- aps/test/index.js
+++ aps/test/index.js
@@ -1,45 +1,5 @@
-import 'dotenv/config.js'
-// import fetch from 'node-fetch'
-import * as uWS from 'uWebSockets.js'
 import * as def from './def.js'
 import * as obj from './obj.js'
-import { checkEv } from '../../ev.js'
-import { logger } from '../../logger.js'
-import Plc from '../../Plc.js'
-import Router from '../../Router.js'
-import Server from '../../Server.js'
+import main from '../../index.js'
 
-const HTTP = Number(process.env.HTTP)
-const IP = process.env.PLC_IP
-const RACK = Number(process.env.PLC_RACK)
-const SLOT = Number(process.env.PLC_SLOT)
-
-const start = async () => {
-  try {
-    const app = uWS.App().listen(HTTP, token => logger.info(token))
-    // db
-    const log = new Server('') // ':memory:' or db name 'sqlite.db'
-    log.run(process.env.SERVER, process.env.TCP)
-    // plc comm
-    const plc01 = new Plc(IP, RACK, SLOT)
-    plc01.run(def, obj)
-    plc01.on('pub', ({ channel, data }) => {
-      if (channel === 'aps/queue') {
-        const { index, queue } = JSON.parse(data)
-        if (queue.length) {
-          checkEv(def, queue[index], obj, plc01)
-        }
-      }
-    })
-    const plc02 = new Plc(IP, RACK, SLOT)
-    plc02.map(def, obj)
-    // api routes
-    const router = new Router(app, log, plc01)
-    router.run(def, obj, `/aps/${process.env.APS}/ev`)
-  } catch (err) {
-    logger.error(new Error(err))
-    process.exit(1)
-  }
-}
-
-start()
+main(def, obj)
