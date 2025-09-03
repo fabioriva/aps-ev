1ed0d0b49eb6e89d852579c99ff2425a2b2d9f64
fabioriva <f.riva@sotefin.ch>
2025-09-03 14:15:08 +0100

    queue management endpoints code refactoring

--- aps/spire/def.js
+++ aps/spire/def.js
@@ -4,12 +4,12 @@ export const SWAP_EV_QUEUE_LEN = 5
 export const SWAP_ST_QUEUE_LEN = 5
 
 const DB_DATA = 541
-const DB_DATA_LEN = 188
+const DB_DATA_LEN = 186
 export const DB_DATA_INIT_DEVICE = 0
-export const DB_DATA_INIT_EXIT_ST_QUEUE = 36
-export const DB_DATA_INIT_EXIT_EV_QUEUE = 74
-export const DB_DATA_INIT_SWAP_EV_QUEUE = 112
-export const DB_DATA_INIT_SWAP_ST_QUEUE = 150
+export const DB_DATA_INIT_EXIT_EV_QUEUE = 38
+export const DB_DATA_INIT_EXIT_ST_QUEUE = 74
+export const DB_DATA_INIT_SWAP_EV_QUEUE = 110
+export const DB_DATA_INIT_SWAP_ST_QUEUE = 148
 
 export const DATA_READ = {
   area: 0x84,
@@ -21,56 +21,50 @@ export const DATA_READ = {
 export const REQ_EXIT_IN = {
   area: 0x84,
   dbNumber: DB_DATA,
-  start: 70,
-  amount: 2,
-  wordLen: 0x02
-}
-export const REQ_EXIT_OUT = {
-  area: 0x84,
-  dbNumber: DB_DATA,
-  start: 72,
-  amount: 2,
-  wordLen: 0x02
-}
-export const REQ_EXIT_EV_IN = {
-  area: 0x84,
-  dbNumber: DB_DATA,
-  start: 108,
+  start: 36,
   amount: 2,
   wordLen: 0x02
 }
 export const REQ_EXIT_EV_OUT = {
   area: 0x84,
   dbNumber: DB_DATA,
-  start: 110,
+  start: 72,
   amount: 2,
   wordLen: 0x02
 }
+export const REQ_EXIT_ST_OUT = {
+  area: 0x84,
+  dbNumber: DB_DATA,
+  start: 108,
+  amount: 2,
+  wordLen: 0x02
+}
+
 export const REQ_SWAP_EV_IN = {
   area: 0x84,
   dbNumber: DB_DATA,
-  start: 146,
+  start: 144,
   amount: 2,
   wordLen: 0x02
 }
 export const REQ_SWAP_EV_OUT = {
   area: 0x84,
   dbNumber: DB_DATA,
-  start: 148,
+  start: 146,
   amount: 2,
   wordLen: 0x02
 }
 export const REQ_SWAP_ST_IN = {
   area: 0x84,
   dbNumber: DB_DATA,
-  start: 184,
+  start: 182,
   amount: 2,
   wordLen: 0x02
 }
 export const REQ_SWAP_ST_OUT = {
   area: 0x84,
   dbNumber: DB_DATA,
-  start: 186,
+  start: 184,
   amount: 2,
   wordLen: 0x02
 }
